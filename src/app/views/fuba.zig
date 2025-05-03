cd819771984b5220dbba25a7b1993baccecc4320
Thomás <thomas.fsr@gmail.com>
2025-05-03 10:32:56 -0300

    fuba views

--- src/app/views/fuba.zig
+++ src/app/views/fuba.zig
@@ -5,11 +5,100 @@ pub fn index(request: *jetzig.Request) !jetzig.View {
     return request.render(.ok);
 }
 
+pub fn get(id: []const u8, request: *jetzig.Request) !jetzig.View {
+    _ = id;
+    return request.render(.ok);
+}
+
+pub fn new(request: *jetzig.Request) !jetzig.View {
+    return request.render(.ok);
+}
+
+pub fn edit(id: []const u8, request: *jetzig.Request) !jetzig.View {
+    _ = id;
+    return request.render(.ok);
+}
+
+pub fn post(request: *jetzig.Request) !jetzig.View {
+    return request.render(.created);
+}
+
+pub fn put(id: []const u8, request: *jetzig.Request) !jetzig.View {
+    _ = id;
+    return request.render(.ok);
+}
+
+pub fn patch(id: []const u8, request: *jetzig.Request) !jetzig.View {
+    _ = id;
+    return request.render(.ok);
+}
+
+pub fn delete(id: []const u8, request: *jetzig.Request) !jetzig.View {
+    _ = id;
+    return request.render(.ok);
+}
+
 
 test "index" {
     var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
     defer app.deinit();
 
     const response = try app.request(.GET, "/fuba", .{});
+    try response.expectStatus(.ok);
+}
+
+test "get" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.GET, "/fuba/example-id", .{});
+    try response.expectStatus(.ok);
+}
+
+test "new" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.GET, "/fuba/new", .{});
+    try response.expectStatus(.ok);
+}
+
+test "edit" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.GET, "/fuba/example-id/edit", .{});
+    try response.expectStatus(.ok);
+}
+
+test "post" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.POST, "/fuba", .{});
+    try response.expectStatus(.created);
+}
+
+test "put" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.PUT, "/fuba/example-id", .{});
+    try response.expectStatus(.ok);
+}
+
+test "patch" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.PATCH, "/fuba/example-id", .{});
+    try response.expectStatus(.ok);
+}
+
+test "delete" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.DELETE, "/fuba/example-id", .{});
     try response.expectStatus(.ok);
 }
