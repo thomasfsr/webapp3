4fac0cf161ae00c6217c84202c45a7e58702c528
Thomás <thomas.fsr@gmail.com>
2025-05-05 23:50:12 -0300

    form

---
+++ src/app/views/form.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const jetzig = @import("jetzig");
+
+pub fn post(request: *jetzig.Request) !jetzig.View {
+    return request.render(.created);
+}
+
+
+test "post" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    const response = try app.request(.POST, "/form", .{});
+    try response.expectStatus(.created);
+}
