bb83591d9bbd36ef704bceeaedac7623f0f82804
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-05-11 23:52:29 -0300

    test

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,6 @@ const zmd = @import("zmd");
 pub const routes = @import("routes");
 pub const static = @import("static");
 
-// Override default settings in `jetzig.config` here:
 pub const jetzig_options = struct {
     /// Middleware chain. Add any custom middleware here, or use middleware provided in
     /// `jetzig.middleware` (e.g. `jetzig.middleware.HtmxMiddleware`).
